b50a72ccb3733cb2a191f40afbf3581b9c35b6c9
Macikolej <oiejnik152@gmail.com>
2022-01-20 18:32:01 +0100

    minor utils changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,20 @@
-import ky from 'ky';
+import ky from "ky";
 
 const apiClient = ky.create({
-  prefixUrl: 'https://rpg-session-organiser-server.herokuapp.com',
+  prefixUrl: "https://rpg-session-organiser-server.herokuapp.com",
 });
 
-export const apiGetDates = () => (
-  apiClient.get('dates').json()
-);
+export const apiGetDates = () => apiClient.get("dates").json();
 
-export const apiGetPropositions = () => (
-  apiClient.get('dates').json()
-);
+export const apiGetPropositions = () => apiClient.get("dates").json();
 
-export const apiPostDate = (accessToken) => (
-  apiClient.post('date', { json: { accessToken: accessToken } }).json()
-);
+export const apiPostDate = (accessToken) =>
+  apiClient.post("date", { json: { accessToken: accessToken } }).json();
 
-export const apiPostProposition = (accessToken) => (
-  apiClient.post('proposition', { json: { accessToken: accessToken } }).json()
-);
+export const apiPostProposition = (accessToken) =>
+  apiClient.post("proposition", { json: { accessToken: accessToken } }).json();
 
-export const apiSignIn = (username, isGm) => (
-  apiClient.post('session', { json: { username: username, is_gm: isGm } }).json()
-);
+export const apiSignIn = (username, isGm) =>
+  apiClient
+    .post("session", { json: { username: username, is_gm: isGm } })
+    .json();
